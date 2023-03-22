98bd97d1317b79efe1fbb391ef2467086f188512
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-22 16:18:05 -0700

    analytics  and payment models are finished

---
+++ server/models/Analytics.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const analyticsSchema = new mongoose.Schema({
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Video',
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    event: {
+      type: String,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+    // Additional fields for analytics metadata
+  });
+  
+  const Analytics = mongoose.model('Analytics', analyticsSchema);
+  
+  module.exports = Analytics;
+  
