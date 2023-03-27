64f0c06d7b4ce49b3fb5c08278d5da00f1d0f13e
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-27 15:53:58 -0700

    added translate and subtitle form

---
+++ client/src/components/SubtitleForm/index.js
@@ -0,0 +1,46 @@
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { ADD_SUBTITLE } from './mutations';
+
+function SubtitleForm() {
+  const [videoId, setVideoId] = useState('');
+  const [language, setLanguage] = useState('');
+  const [text, setText] = useState('');
+  const [file, setFile] = useState(null);
+
+  const [addSubtitle, { loading, error }] = useMutation(ADD_SUBTITLE);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await addSubtitle({
+        variables: { videoId, language, text, file },
+      });
+      console.log('Subtitle added:', result.data.addSubtitle);
+      // reset form fields after successful submission
+      setVideoId('');
+      setLanguage('');
+      setText('');
+      setFile(null);
+    } catch (error) {
+      console.error('Error adding subtitle:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="videoId">Video ID:</label>
+      <input type="text" id="videoId" value={videoId} onChange={(event) => setVideoId(event.target.value)} />
+      <label htmlFor="language">Language:</label>
+      <input type="text" id="language" value={language} onChange={(event) => setLanguage(event.target.value)} />
+      <label htmlFor="text">Text:</label>
+      <textarea id="text" value={text} onChange={(event) => setText(event.target.value)} />
+      <label htmlFor="file">Subtitle file:</label>
+      <input type="file" id="file" onChange={(event) => setFile(event.target.files[0])} />
+      <button type="submit" disabled={loading}>Submit</button>
+      {error && <p>Error: {error.message}</p>}
+    </form>
+  );
+}
+
+export default SubtitleForm;
---
+++ client/src/components/TranslateForm/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { TRANSLATE_SUBTITLE } from './mutations';
+import { useMutation } from '@apollo/client';
+import { QUERY_ALL_SUBTITLES } from './queries';
+
+function TranslateSubtitleForm(thisVideoId) {
+  const [subtitleId, setSubtitleId] = useState('');
+  const [text, setText] = useState('');
+  const [language, setLanguage] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+  
+  const [translateSubtitle] = useMutation(TRANSLATE_SUBTITLE, {
+    update(cache, { data: { translateSubtitle } }) {
+      const { getSubtitles } = cache.readQuery({ query: QUERY_ALL_SUBTITLES, variables: { videoId: thisVideoId } });
+      const updatedSubtitles = getSubtitles.map(subtitle => {
+        if (subtitle.id === translateSubtitle.id) {
+          return translateSubtitle;
+        } else {
+          return subtitle;
+        }
+      });
+      cache.writeQuery({ query: QUERY_ALL_SUBTITLES, variables: { videoId: thisVideoId }, data: { getSubtitles: updatedSubtitles } });
+    }
+  });
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    translateSubtitle({ variables: { subtitleId, language, translatedText } });
+    setSubtitleId('');
+    setText('');
+    setLanguage('');
+    setTranslatedText('');
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="subtitleId">Subtitle ID:</label>
+      <input type="text" id="subtitleId" value={subtitleId} onChange={(e) => setSubtitleId(e.target.value)} />
+
+      <label htmlFor="text">Original Text:</label>
+      <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} />
+
+      <label htmlFor="language">Language:</label>
+      <input type="text" id="language" value={language} onChange={(e) => setLanguage(e.target.value)} />
+
+      <label htmlFor="translatedText">Translated Text:</label>
+      <input type="text" id="translatedText" value={translatedText} onChange={(e) => setTranslatedText(e.target.value)} />
+
+      <button type="submit">Translate Subtitle</button>
+    </form>
+  );
+}
+
+export default TranslateSubtitleForm;
